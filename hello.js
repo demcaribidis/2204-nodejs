ab88330eea1ef66321a0a22cdb2fc7665e2ef92a
Robert Mazur <105283980+rob-mazur@users.noreply.github.com>
2022-06-19 14:14:45 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,1 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
-
-console.log('Server started');
+console.log("Hello World");
